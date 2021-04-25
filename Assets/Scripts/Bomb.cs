5d7834c29f3f63d722d769ebd37e254eff84b20f
fraanns <julianntbr50@googlemail.com>
2021-04-26 00:36:26 +0200

    Fixed bombs not dropping gold

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -6,8 +6,9 @@ public class Bomb : MonoBehaviour
 {
     public int damage;
 
-
+    public GameObject tileBreakEffect;
     public GameObject explosionPrefab;
+    public GameObject droppedGoldPrefab;
 
     float timer = 2;
 
@@ -31,6 +32,16 @@ public class Bomb : MonoBehaviour
                 Tile tile = chunk.GetTile(tilePos);
                 tile.isAir = true;
                 chunk.SetTile(tilePos, tile);
+
+                for (int t = 0; t < tile.goldAmount; t++)
+                {
+                    GameObject gold = Instantiate(droppedGoldPrefab, new Vector3(tilePos.x + Random.Range(0f, 1f), tilePos.y + Random.Range(0f, 1f), 0), Quaternion.identity);
+                    gold.GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(-5f, 5f), Random.Range(1f, 3f)));
+                }
+
+                GameObject effect = Instantiate(tileBreakEffect, new Vector3(tilePos.x + Random.Range(0f, 1f), tilePos.y + Random.Range(0f, 1f), 0), Quaternion.identity);
+                Destroy(effect, 3);
+
 
                 WorldController.controller.SetChunk(chunk);
 
